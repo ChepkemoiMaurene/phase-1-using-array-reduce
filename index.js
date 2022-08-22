96cf2f6c8c1859252be43c877ca7d2be199fcd56
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2022-08-22 14:37:24 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
 
 // Code your solution here
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+
+
+// 4 + 5 + 3 + 4 + 4 + 6 + 5
+const initialValue = 0;
+const totalBatteries = batteryBatches .reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);
+
+console.log(totalBatteries);
+// expected output: 31
